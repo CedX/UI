6feb7394f39f7968cf54177589de566fa6f78db8
Cédric Belin <cedx@outlook.com>
2026-06-14 03:03:22 +0200

    Fix the "Test" script

--- Scripts/Test.ps1
+++ Scripts/Test.ps1
@@ -1,9 +1,8 @@
 using module ./Cmdlets.psm1
+& "$PSScriptRoot/Build.ps1"
 
 "Running the test suite..."
-Invoke-TypeScript Sources/Client/tsconfig.json -SourceMap
 Invoke-NodeTest
-
 pwsh -Command {
 	Import-Module Pester
 	Invoke-Pester Tests
