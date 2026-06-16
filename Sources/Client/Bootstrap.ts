3c02edd2c6934f259d1d5bdf428bb5afec52d5cf
Cédric Belin <cedx@outlook.com>
2026-06-16 15:21:13 +0200

    Remove the `disposeTabs()` function

--- Sources/Client/Bootstrap.ts
+++ Sources/Client/Bootstrap.ts
@@ -1,4 +1,4 @@
-import {Popover, Tab, Tooltip} from "bootstrap";
+import {Popover, Tooltip} from "bootstrap";
 
 /**
  * Disposes all popovers attached to the specified element.
@@ -6,14 +6,6 @@ import {Popover, Tab, Tooltip} from "bootstrap";
  */
 export function disposePopovers(element: Element): void {
 	for (const node of element.querySelectorAll('[data-bs-toggle="popover"]')) Popover.getInstance(node)?.dispose();
-}
-
-/**
- * Disposes all tabs attached to the specified element.
- * @param element The source element.
- */
-export function disposeTabs(element: Element): void {
-	for (const node of element.querySelectorAll('[data-bs-toggle="tab"]')) Tab.getInstance(node)?.dispose();
 }
 
 /**
