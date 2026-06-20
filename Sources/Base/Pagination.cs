f6f6cb58ed7898881a3c2173b38c53c9268ea111
Cédric Belin <cedx@outlook.com>
2026-06-20 13:29:36 +0200

    Add the `PaginatedList` class

--- Sources/Base/Pagination.cs
+++ Sources/Base/Pagination.cs
@@ -52,3 +52,24 @@ public sealed class Pagination {
 	/// </summary>
 	public int TotalItemCount { get; set => field = Math.Max(0, value); }
 }
+
+/// <summary>
+/// A list with information relevant to the pagination of its items.
+/// </summary>
+/// <typeparam name="T">The type of items in the list.</typeparam>
+public class PaginatedList<T>: List<T> {
+
+	/// <summary>
+	/// The information relevant to the pagination of list items.
+	/// </summary>
+	public Pagination Pagination { get; set; } = new();
+
+	/// <summary>
+	/// Creates an empty paginated list.
+	/// </summary>
+	/// <param name="itemsPerPage">The number of items per page.</param>
+	/// <returns>An empty paginated list with the specified number of items per page.</returns>
+	public static PaginatedList<T> Empty(int itemsPerPage) => new() {
+		Pagination = new() { ItemsPerPage = itemsPerPage }
+	};
+}
