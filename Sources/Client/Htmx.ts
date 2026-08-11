35cd3da5a39ea7471d316578bd19f0e94ce2de27
Cédric Belin <cedx@outlook.com>
2026-08-11 10:32:14 +0200

    Add the `failed()` and `successful()` functions to the `Htmx` module

--- Sources/Client/Htmx.ts
+++ Sources/Client/Htmx.ts
@@ -11,3 +11,23 @@ export type HtmxEventDetail = HtmxResponseInfo & {
 	 */
 	elt: Element;
 };
+
+/**
+ * Returns a value indicating whether the specified response failed.
+ * @param response The `htmx` response.
+ * @returns `true` if the specified response failed, otherwise `false`.
+ */
+export function failed(response: HtmxResponseInfo): boolean {
+	const {status} = response.xhr;
+	return Boolean(response.failed) || (status >= 400 && status < 600);
+}
+
+/**
+ * Returns a value indicating whether the specified response was successful.
+ * @param response The `htmx` response.
+ * @returns `true` if the specified response was successful, otherwise `false`.
+ */
+export function successful(response: HtmxResponseInfo): boolean {
+	const {status} = response.xhr;
+	return Boolean(response.successful) || (status >= 200 && status < 300);
+}
