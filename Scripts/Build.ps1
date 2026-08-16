0603106d508014034e9c16ccf591cdbd0061799e
Cédric Belin <cedx@outlook.com>
2026-08-16 15:22:14 +0200

    Code formatting [skip ci]

--- Scripts/Build.ps1
+++ Scripts/Build.ps1
@@ -2,6 +2,4 @@ using module ./Cmdlets.psm1
 
 "Building the solution..."
 Build-DotNetSolution ($Release ? "Release" : "Debug")
-
-$sourceMap = -not $Release
-Invoke-TypeScript Sources/Client/tsconfig.json -SourceMap:$sourceMap
+Invoke-TypeScript Sources/Client/tsconfig.json -SourceMap:(-not $Release)
