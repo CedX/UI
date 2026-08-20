d38b57d98fa5f63d83a8cef16cd5c2359a695060
Cédric Belin <cedx@outlook.com>
2026-08-20 10:53:13 +0200

    Add the `round()` function

---
+++ Tests/Client/Number.Tests.js
@@ -0,0 +1,15 @@
+import {round} from "@cedx/ui/Number.js";
+import {assert} from "chai";
+
+/**
+ * Tests the features of the number functions.
+ */
+describe("Number", () => {
+	describe("round()", () => {
+		it("should round the specified value to the given precision", () => {
+			assert.equal(round(123.456, 0), 123);
+			assert.equal(round(123.456, 1), 123.5);
+			assert.equal(round(123.456, 2), 123.46);
+		});
+	});
+});
