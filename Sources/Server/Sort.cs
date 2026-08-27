13b541a27b099d723e4594e60d08bd0a070a01f9
Cédric Belin <cedx@outlook.com>
2026-08-27 19:37:06 +0200

    Remove useless implicit conversions

--- Sources/Server/Sort.cs
+++ Sources/Server/Sort.cs
@@ -1,8 +1,5 @@
 namespace Belin.UI;
 
-using System.Collections;
-using System.Collections.Specialized;
-
 /// <summary>
 /// Represents information relevant to the sorting of data items.
 /// </summary>
@@ -32,14 +29,6 @@ public sealed class Sort(IEnumerable<KeyValuePair<string, SortDirection>>? prope
 	/// </summary>
 	/// <param name="columns">The array whose elements are copied to the order hint collection.</param>
 	/// <returns>The order hint collection corresponding to the specified array of column names.</returns>
-	public static implicit operator Sort(object?[] columns) =>
-		new(columns.Select(value => new KeyValuePair<string, SortDirection>(value?.ToString() ?? "", SortDirection.Ascending)));
-
-	/// <summary>
-	/// Creates a new order hint collection from the specified array of column names.
-	/// </summary>
-	/// <param name="columns">The array whose elements are copied to the order hint collection.</param>
-	/// <returns>The order hint collection corresponding to the specified array of column names.</returns>
 	public static implicit operator Sort(string[] columns) =>
 		new(columns.Select(value => new KeyValuePair<string, SortDirection>(value, SortDirection.Ascending)));
 
@@ -50,16 +39,6 @@ public sealed class Sort(IEnumerable<KeyValuePair<string, SortDirection>>? prope
 	/// <returns>The order hint collection corresponding to the specified array of column names.</returns>
 	public static implicit operator Sort(List<string> columns) =>
 		new(columns.Select(value => new KeyValuePair<string, SortDirection>(value, SortDirection.Ascending)));
-
-	/// <summary>
-	/// Creates a new order hint collection from the specified dictionary of column names and sort orders.
-	/// </summary>
-	/// <param name="orderHints">The dictionary whose elements are copied to the order hint collection.</param>
-	/// <returns>The order hint collection corresponding to the specified dictionary of column names and sort orders.</returns>
-	public static implicit operator Sort(OrderedDictionary orderHints) => new(orderHints.Cast<DictionaryEntry>().Select(entry => {
-		var value = entry.Value is SortDirection sortDirection ? sortDirection : Enum.Parse<SortDirection>(entry.Value?.ToString() ?? "", ignoreCase: true);
-		return new KeyValuePair<string, SortDirection>(entry.Key.ToString() ?? "", value);
-	}));
 
 	/// <summary>
 	/// Gets the icon corresponding to the specified property.
