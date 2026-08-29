a6ab4e2b54fb2709aae47a274d5940e18b5e7cb2
Cédric Belin <cedx@outlook.com>
2026-08-29 16:34:25 +0200

    Module merging

--- Tests/Client/Sort.Tests.js
+++ Tests/Client/Sort.Tests.js
@@ -1,6 +1,5 @@
 /* eslint-disable max-lines-per-function */
-import {Sort} from "@cedx/ui/Sort.js";
-import {SortDirection} from "@cedx/ui/SortDirection.js";
+import {Sort, SortDirection} from "@cedx/ui/Sort.js";
 import {assert} from "chai";
 
 /**
