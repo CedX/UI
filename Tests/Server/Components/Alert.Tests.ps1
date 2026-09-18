d0972b9a72f5622cb9970d446e82733e42328d2e
Cédric Belin <cedx@outlook.com>
2026-09-18 22:39:16 +0200

    Fix the tests

--- Tests/Server/Components/Alert.Tests.ps1
+++ Tests/Server/Components/Alert.Tests.ps1
@@ -6,30 +6,30 @@ using module ../../../UI.psd1
 #>
 Describe "New-Alert" {
 	It 'should handle the "Class" parameter' {
-		Should-BeLikeString "*mb-0 rounded-0*" (uiAlert -Class mb-0, rounded-0) -CaseSensitive
+		Should-BeLikeString "*mb-0 rounded-0*" (New-UIAlert -Class mb-0, rounded-0) -CaseSensitive
 	}
 
 	It 'should handle the "Context" parameter' {
-		$alert = uiAlert -Context danger
+		$alert = New-UIAlert -Context danger
 		Should-BeLikeString "*alert-danger*" $alert -CaseSensitive
 		Should-BeLikeString "*<i*>error</i>*" $alert -CaseSensitive
 	}
 
 	It 'should handle the "Dismissible" parameter' {
-		$alert = uiAlert -Dismissible
+		$alert = New-UIAlert -Dismissible
 		Should-BeLikeString "*alert-dismissible*" $alert -CaseSensitive
 		Should-BeLikeString '*data-bs-dismiss="alert"*' $alert -CaseSensitive
 	}
 
 	It 'should handle the "Fade" parameter' {
-		Should-BeLikeString "*fade show*" (uiAlert -Fade) -CaseSensitive
+		Should-BeLikeString "*fade show*" (New-UIAlert -Fade) -CaseSensitive
 	}
 
 	It 'should handle the "Icon" parameter' {
-		Should-BeLikeString "*<i*>heart</i>*" (uiAlert -Icon heart) -CaseSensitive
+		Should-BeLikeString "*<i*>heart</i>*" (New-UIAlert -Icon heart) -CaseSensitive
 	}
 
 	It "should handle the inner content" {
-		Should-BeLikeString '*<input type="text">*' (uiAlert { input -Type text }) -CaseSensitive
+		Should-BeLikeString '*<input type="text">*' (New-UIAlert { input -Type text }) -CaseSensitive
 	}
 }
